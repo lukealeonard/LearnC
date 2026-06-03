33b1d3fa9cd0159e8fb01bcf4a4dd79143f0166a
lukealeonard <lukealeonard@proton.me>
2026-06-02 21:15:07 -0400

    not sure

--- Challenges - Level 1/Beginner/ReverseAString.c
+++ Challenges - Level 1/Beginner/ReverseAString.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(){
 
         system("clear");
+        printf("REVERSE A STRING\n");
+        printf("you will be prompted to enter a sentence.\n");
 
-        double prices[] = {5.0, 10.0, 15.0, 25.0, 20.0}; 
-        printf("%.1lf\n", prices[0]);
+        sleep(3);
+        system("clear");
+
+        char sentence[100] = "";
+        printf("please type a sentence:\n");
+        fgets(sentence, sizeof(sentence), stdin);
+        int length = strlen(sentence);
+
+        sleep(1);
+        system("clear");
+        printf("Your string backwards is...\n");
+        sleep(2);
+        system("clear");
+
+        for (int i = (--length); i == 0; i--){
+                printf("%c", sentence[i]);
+                usleep(250000);
+        }
+
+        printf("\n");
 
         return 0;
 }
